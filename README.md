f2f83b59ad01841d2c7f49f6a1b13e92cff881fc
Jim Buitt <jim.buitt@gmail.com>
2016-05-14 14:51:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# nwws-perl-client
+
+A simple client for the NWWS-2 OI (NOAA Weather Wire Service version 2 Open Interface) written in Perl. The NOAA Weather Wire Service is a satellite data collection and dissemination system operated by the [National Weather Service](http://weather.gov), which was established in October 2000. Its purpose is to provide state and federal government, commercial users, media and private citizens with timely delivery of meteorological, hydrological, climatological and geophysical information. 
+
+This client was largely based on the example script found in the [Net::Jabber Perl module](http://search.cpan.org/~reatmon/Net-Jabber-2.0/lib/Net/Jabber.pm) documentation directory `/usr/share/doc/libnet-jabber-perl/examples/client.pl`.
+
+####How do I run it?
+This script was developed and tested on [Ubuntu 14.04](http://ubuntu.com). After downloading the latest [release](https://github.com/jbuitt/nwws-perl-client), run the following command to install the dependencies:
+
+```
+$ sudo apt-get -y install libnet-jabber-perl libconfig-inifiles-perl libxml-simple-perl
+```
+
+Now create a config file: (e.g. config.ini)
+
+```
+[Main]
+server=nwws-oi.weather.gov
+port=5222
+username=[nwws_username]
+password=[nwws_paswword]
+resource=some_resource
+logfile=/path/to/log/file
+archivedir=/path/to/archive
+```
+
+Where [nwws_username] and [nwws_password] are your NWWS-2 credentials obtained by signing up [on the NOAA Weather Wire Service website](http://www.nws.noaa.gov/nwws/#NWWS_OI_Request).
+
+NOTE: Despite the NWWS-2 website's instructions to use port 5223, this client uses TLS on port 5222. Support for SSL on port 5223 was buggy within the client, so I removed it.
+
+Now run the script:
+
+```
+$ ./nwws2.pl /path/to/config.ini
+```
+
+Provided that you're able to connect to the NWWS and your credentials are accepted, you will start to see products appear in the supplied archive directory.
+
+####Author
+
++	[jim.buitt at gmail.com](mailto:jim.buitt@gmail.com)
+
+## License
+
+See [LICENSE](https://github.com/jbuitt/nwws-perl-client/blob/master/LICENSE) file.
+
---
+++ nwws2.pl
@@ -0,0 +1,200 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Net::Jabber;
+use Config::IniFiles;
+use XML::Simple;
+use Data::Dumper;
+
+$Data::Dumper::Sortkeys = 1;
+
+die "Usage: $0 /path/to/config/file\n" if $#ARGV < 0;
+my $config_file = $ARGV[0];
+
+my $cfg = Config::IniFiles->new( -file => $config_file );
+
+my $server   = $cfg->val('Main', 'server');
+my $port     = $cfg->val('Main', 'port');
+my $username = $cfg->val('Main', 'username') . '@' . $cfg->val('Main', 'server');
+my $password = $cfg->val('Main', 'password');
+my $resource = $cfg->val('Main', 'resource');
+
+$SIG{HUP} = \&Stop;
+$SIG{KILL} = \&Stop;
+$SIG{TERM} = \&Stop;
+$SIG{INT} = \&Stop;
+
+our $Connection;
+
+#Start connect loop
+while(1) {
+
+	#Create new Jabber client
+	$Connection = new Net::Jabber::Client();
+
+	#Turn on debug messages
+	#$Connection->{DEBUG}->Init(level=>3, file=>"stdout");
+
+	# Set up callback functions
+	$Connection->SetCallBacks(
+		message  => \&InMessage,
+		presence => \&InPresence,
+		iq       => \&InIQ
+	);
+
+	# Connect to server
+	my $status = $Connection->Connect(
+		hostname => $server,
+		port     => $port,
+		_tls     => 1
+	);
+
+	if (!(defined($status))) {
+		printToLog("ERROR:  Jabber server is down or connection was not allowed.");
+		printToLog("($!)");
+		exit(0);
+	}
+
+	# Send auth info
+	my @result = $Connection->AuthSend(
+		username => $username,
+		password => $password,
+		resource => $resource
+	);
+
+	if ($result[0] ne "ok") {
+		printToLog("ERROR: Authorization failed: $result[0] - $result[1]");
+		exit(0);
+	}
+
+	printToLog("Logged in to $server:$port...");
+
+	printToLog("Getting Roster to tell server to send presence info...");
+	$Connection->RosterGet();
+
+	printToLog("Sending presence to tell world that we are logged in...");
+	$Connection->PresenceSend();
+
+	# Join the 'nwws' conference room
+	$Connection->MUCJoin(
+		room   => 'nwws',
+		server => 'conference.' . $cfg->val('Main', 'server'),
+		nick   => $cfg->val('Main', 'resource')
+	);
+
+	# Enter event loop
+	while(defined($Connection->Process())) { }
+
+	# If we got here, the connection was severed
+	printToLog("ERROR: The connection was killed, restarting.");
+	
+	# Sleep for 1 second
+	sleep(1);
+}
+
+# Done
+exit(0);
+
+################################################################################
+sub Stop {
+	printToLog("Exiting...");
+	$Connection->Disconnect();
+	exit(0);
+}
+
+################################################################################
+sub InMessage {
+	my $sid = shift;
+	my $message = shift;
+	my $type = $message->GetType();
+	my $fromJID = $message->GetFrom("jid");
+	my $from = $fromJID->GetUserID();
+	my $resource = $fromJID->GetResource();
+	my $subject = $message->GetSubject();
+	my $body = $message->GetBody();
+	return if $body =~ /^\*\*WARNING\*\*/;
+	return if $body =~ /issues  valid/;
+	return if $body =~ /issues TST valid/;
+	my $xml = $message->GetXML();
+	printToLog("===");
+	printToLog("Message ($type)");
+	printToLog("  From: $from ($resource)");
+	printToLog("  Subject: $subject");
+	printToLog("  Body: $body");
+	#printToLog("===");
+	#printToLog($xml);
+	#printToLog("===");
+	my $ref = XMLin($xml);
+	#print Dumper($ref);
+	return if !defined($ref->{'body'});
+	#print $ref->{'body'} . "\n";
+	# Create archive dir if it does not exist
+	my $newref;
+	if (ref($ref->{'x'}) eq 'ARRAY') {
+		$newref = $ref->{'x'}->[0];
+	} elsif (ref($ref->{'x'}) eq 'HASH') {
+		$newref = $ref->{'x'};
+	}
+	if (! -d $cfg->val('Main', 'archivedir')) {
+		mkdir($cfg->val('Main', 'archivedir')) or die "Could not create archive directory: $!\n";
+	}
+	if (! -d $cfg->val('Main', 'archivedir') . '/' . $newref->{'cccc'}) {
+		mkdir($cfg->val('Main', 'archivedir') . '/' . $newref->{'cccc'}) or die "Could not create WFO directory: $!\n";
+	}
+	# create product file
+	my $prodDate = $newref->{'issue'};
+	$prodDate =~ s/[\-T\:]//g;
+	$prodDate =~ s/Z$//;
+	my $file = $newref->{'awipsid'} . '-' . $newref->{'id'} . '-' . $prodDate . '.txt';
+	open(OUTFILE, ">" . $cfg->val('Main', 'archivedir') . '/' . $newref->{'cccc'} . '/' . $file) or die $!;
+	my @lines = split(/\n/, $newref->{'content'});
+	for(my $i=0; $i<@lines; $i+=2) {
+		#next if $lines[$i] !~ /^\s*$/;
+		#last if !defined($lines[$i]);
+		print OUTFILE $lines[$i] . "\n";
+	}
+	close(OUTFILE);
+}
+
+################################################################################
+sub InIQ {
+	my $sid = shift;
+	my $iq = shift;
+	my $from = $iq->GetFrom();
+	my $type = $iq->GetType();
+	my $query = $iq->GetQuery();
+	my $xmlns = $query->GetXMLNS();
+	printToLog("===");
+	printToLog("IQ");
+	printToLog("  From $from");
+	printToLog("  Type: $type");
+	printToLog("  XMLNS: $xmlns");
+	printToLog("===");
+	printToLog($iq->GetXML());
+	printToLog("===");
+}
+
+################################################################################
+sub InPresence {
+	my $sid = shift;
+	my $presence = shift;
+	my $from = $presence->GetFrom();
+	my $type = $presence->GetType();
+	my $status = $presence->GetStatus();
+	#printToLog("===");
+	#printToLog("Presence");
+	#printToLog("  From $from");
+	#printToLog("  Type: $type");
+	#printToLog("  Status: $status");
+	#printToLog("===");
+	#printToLog($presence->GetXML());
+	#printToLog("===");
+}
+
+################################################################################
+sub printToLog {
+	my $logMsg = shift;
+	open(LOG, '>>' . $cfg->val('Main', 'logfile')) or die $!;
+	print LOG $logMsg . "\n";
+	close(LOG);
+}
