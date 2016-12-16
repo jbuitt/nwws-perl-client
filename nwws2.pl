026deba2779a86e59bd28982927958826b3e22c3
Jim Buitt <jim.buitt@gmail.com>
2016-12-16 07:19:57 -0600

    fixed issue with skipped parsing product

--- nwws2.pl
+++ nwws2.pl
@@ -167,8 +167,8 @@ sub InMessage {
 	my $newId = substr($rightnow_gmt[5]+1900, 0, 2) . (length($rightnow_gmt[2]) == 2 ? '0' . $rightnow_gmt[2] : $rightnow_gmt[2]) . (length($rightnow_gmt[1]) == 2 ? '0' . $rightnow_gmt[1] : $rightnow_gmt[1]) . '_' . substr(time(), 0, 3) . substr($tmpArray[1], 0, 5);
 	my $file = lc($newref->{'cccc'}) . '_' . lc($newref->{'ttaaii'}) . '-' . lc($newref->{'awipsid'}) . '.' . $newId . '.txt';
 	open(OUTFILE, '>' . $cfg->val('Main', 'archivedir') . '/' . lc($newref->{'cccc'}) . '/' . $file) or die $!;
-	my @lines = split("/\n/", $newref->{'content'});
-	for(my $i=0; $i<@lines; $i+=2) {
+	my @lines = split(/\n\n/, $newref->{'content'});
+	for(my $i=0; $i<@lines; $i++) {
 		print OUTFILE $lines[$i] . "\n";
 	}
 	close(OUTFILE);
@@ -176,7 +176,8 @@ sub InMessage {
 	if (defined($cfg->val('Main', 'panrun'))) {
 		if ( -x $cfg->val('Main', 'panrun')) {
 			chomp(my $cwd = `pwd`);
-			my @output = `$cfg->val('Main', 'panrun') . ' ' . $cwd . '/' . $cfg->val('Main', 'archivedir') . '/' . $newref->{'cccc'} . '/' . $file . ' 2>&1 &`;
+			my $cmd = $cfg->val('Main', 'panrun') . ' ' . $cwd . '/' . $cfg->val('Main', 'archivedir') . '/' . $newref->{'cccc'} . '/' . $file . ' 2>&1 &';
+			my @output = `$cmd`;
 			if (scalar(@output) == 0) {
 				printToLog("Error running PAN executable: " . join(" ", @output));
 			}
@@ -229,128 +230,3 @@ sub printToLog {
 	print LOG $logMsg . "\n";
 	close(LOG);
 }
-
-__DATA__
-$VAR1 = {
-          'body' => 'KSGX issues MWW valid 2016-12-16T04:26:00Z',
-          'from' => 'nwws@conference.nwws-oi.weather.gov/nwws-oi',
-          'html' => {
-                    'body' => {
-                              'content' => 'KSGX issues MWW valid 2016-12-16T04:26:00Z',
-                              'xmlns' => 'http://www.w3.org/1999/xhtml'
-                            },
-                    'xmlns' => 'http://jabber.org/protocol/xhtml-im'
-                  },
-          'to' => 'jim.buitt@nwws-oi.weather.gov/wxnotify',
-          'type' => 'groupchat',
-          'x' => {
-                 'awipsid' => 'MWWSGX',
-                 'cccc' => 'KSGX',
-                 'content' => '
-
-839
-
-WHUS76 KSGX 160426
-
-MWWSGX
-
-
-
-URGENT - MARINE WEATHER MESSAGE
-
-NATIONAL WEATHER SERVICE SAN DIEGO CA
-
-826 PM PST THU DEC 15 2016
-
-
-
-...VERY STRONG WINDS AND LARGE SEAS FRIDAY AND FRIDAY NIGHT...
-
-
-
-.GUSTY SOUTHWEST WINDS FROM 15 TO 20 KT TONIGHT WILL SHIFT TO THE
-
-WEST WITH THE PASSAGE OF A STRONG COLD FRONT FRIDAY MORNING. THE
-
-STRONGEST WINDS AND LARGEST SEAS ARE EXPECTED FRIDAY AFTERNOON
-
-AND FRIDAY NIGHT WITH SUSTAINED WINDS OF 20 TO 30 KT. GUSTS MAY
-
-APPROACH GALE FORCE. THE STRONG WINDS...LARGE COMBINED SEAS...AND
-
-STEEP WAVES WILL CREATE DANGEROUS BOATING CONDITIONS. WINDS AND
-
-SEAS WILL DECREASE SATURDAY.
-
-
-
-PZZ750-775-161400-
-
-/O.CON.KSGX.SC.Y.0021.161216T0800Z-161217T1600Z/
-
-COASTAL WATERS FROM SAN MATEO POINT TO THE MEXICAN BORDER AND OUT
-
-TO 30 NM-
-
-WATERS FROM SAN MATEO POINT TO THE MEXICAN BORDER EXTENDING 30 TO
-
-60 NM OUT INCLUDING SAN CLEMENTE ISLAND-
-
-826 PM PST THU DEC 15 2016
-
-
-
-...SMALL CRAFT ADVISORY REMAINS IN EFFECT FROM MIDNIGHT TONIGHT
-
-TO 8 AM PST SATURDAY...
-
-
-
-* WINDS...SOUTH WINDS 15 TO 20 KT TONIGHT...BECOMING WEST 20 TO 30
-
-  KT FRIDAY MORNING. STRONGEST WINDS WILL OCCUR FRIDAY AFTERNOON
-
-  AND EVENING WITH A FEW GUSTS APPROACHING GALE FORCE.
-
-
-
-* WAVES/SEAS...COMBINED SEAS 8 TO 12 FT OVER THE OUTER COASTAL
-
-  WATERS AND 6 TO 10 FT OVER THE INNER COASTAL WATERS ON FRIDAY.
-
-
-
-PRECAUTIONARY/PREPAREDNESS ACTIONS...
-
-
-
-A SMALL CRAFT ADVISORY MEANS THAT WIND SPEEDS OF 21 TO 33 KNOTS
-
-AND COMBINED SEAS OF 10 FEET OR GREATER ARE EXPECTED TO PRODUCE
-
-HAZARDOUS WAVE CONDITIONS TO SMALL CRAFT. INEXPERIENCED
-
-MARINERS...ESPECIALLY THOSE OPERATING SMALLER VESSELS SHOULD
-
-AVOID NAVIGATING IN THESE CONDITIONS.
-
-
-
-&&
-
-
-
-$$
-
-
-
-MOEDE
-
-',
-                 'id' => '3328.10565',
-                 'issue' => '2016-12-16T04:26:00Z',
-                 'ttaaii' => 'WHUS76',
-                 'xmlns' => 'nwws-oi'
-               }
-        };
-Use of uninitialized value in concatenation (.) or string at ./nwws2.pl line 146.
