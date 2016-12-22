3c82a59bd4261572df01d0166f4d367c96785f6d
Jim Buitt <jim.buitt@gmail.com>
2016-12-22 09:48:48 -0600

    Updated data directory location

--- contrib/bin/archive_products.sh
+++ contrib/bin/archive_products.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-. /etc/default/nwws2
 if [ "$1" = "" ]; then
 	echo "Usage: $0 <days>"
 	exit 1
 fi
+if [ -e "/etc/default/nwws"]; then
+	. /etc/default/nwws
+fi
+if [ "$BASEDIR" = "" ]; then
+	echo "Error: \$BASEDIR is not set."
+	exit 1
+fi
+if [ "$DATADIR" = "" ]; then
+	echo "Error: \$DATADIR is not set."
+	exit 1
+fi
 ARCHIVE_FILE=$(date +"%Y%m%d.tar.gz")
+# Create archive directory if it doesn't exist
+if [ ! -d "$BASEDIR/archive/" ]; then
+	mkdir $BASEDIR/archive/
+fi
 # Create tar.gz backup of products
-/usr/bin/find $BASEDIR/archive/ -type f +mtime $1 -print0 | tar -czvf $BASEDIR/$ARCHIVE_FILE --null -T -
-# Delete products
-/usr/bin/find $BASEDIR/archive/ -type f +mtime $1 -delete
+/usr/bin/find $DATADIR -type f +mtime $1 -print0 | tar -czvf $BASEDIR/archive/$ARCHIVE_FILE --null -T -
 # Done
 exit 0
